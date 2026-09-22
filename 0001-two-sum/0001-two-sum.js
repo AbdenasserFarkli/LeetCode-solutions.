841da4d2417161ea71200840152907f7737be522
AbdenasserFarkli <abdenasserfarkli@gmail.com>
2026-09-22 12:15:59 +0100

    Time: 118 ms (5.04%), Space: 54.5 MB (65.43%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+          let box=[]
+for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+        if (i==j) {
+            continue
+            
+        }else
+            if (nums[i]+nums[j]==target) {
+                box.push(i,j)
+                return box
+
+                
+            }
+        
+    }
+  
+    
+}    
+    
+};
